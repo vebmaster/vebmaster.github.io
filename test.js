8fd9c4835f9c1ad1673105026973c84e94ae4cbb
vebmaster <vebmaster@local>
2019-01-28 21:40:31 +0300

    Меню

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
+jQuery(document).ready(function($)
+{
+    $('ul.navbar-nav').before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
+});
+
+
+
+function callback(parameter)
+{
+    var callback_tag = document.getElementById(parameter);
+    if (callback_tag.style.display == '') {
+        callback_tag.style.display = 'none';
+        return;
+    }
+    callback_tag.style.display = '';
+}
 
