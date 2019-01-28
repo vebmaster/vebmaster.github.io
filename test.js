cd45c989ae3221f63b012ca510adffc17b888668
vebmaster <vebmaster@local>
2019-01-28 21:56:20 +0300

    Меню

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function($)
 {
+    alert('11');
     $("head").append("<link id='fromScript' rel='stylesheet' href='https://vebmaster.github.io/test.css?nocache' type='text/css'>");
     $("ul.navbar-nav").before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
 });
 
 document.addEventListener("DOMContentLoaded", function(event)
 {
+    alert('22');
     $("head").append("<link id='fromScript2' rel='stylesheet' href='https://vebmaster.github.io/test.css?nocache' type='text/css'>");
     $("ul.navbar-nav").before( '<i class="fa fa-bars 222" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
 });
