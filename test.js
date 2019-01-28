8d2c8a28fc6866eefae5d3f4fb42b64e6d0fa743
vebmaster <vebmaster@local>
2019-01-28 21:45:43 +0300

    Меню

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($)
 {
-    $('ul.navbar-nav').before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
+    $("head").append("<link id='fromScript' rel='stylesheet' href='https://vebmaster.github.io/test.css?nocache' type='text/css'>");
+    $("ul.navbar-nav").before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
 });
 
 
