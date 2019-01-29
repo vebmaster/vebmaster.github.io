5f8c405c9789ba742e4773ad5450aff0d5b8f3a5
vebmaster <vebmaster@local>
2019-01-29 11:05:44 +0300

    Меню

--- custom-script.js
+++ custom-script.js
@@ -1,4 +1,7 @@
-alert('custom-script');
+jQuery(document).ready(function($)
+{
+    alert('custom-script');
 
-$("body").append("<link id='fromScript' rel='stylesheet' href='https://vebmaster.github.io/test.css?nocache' type='text/css'>");
-$("ul.navbar-nav").before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
+    $("body").append("<link id='fromScript' rel='stylesheet' href='https://vebmaster.github.io/test.css?nocache' type='text/css'>");
+    $("ul.navbar-nav").before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
+});
