8537682daf5b8bdef3c990abbb060502657f9046
vebmaster <vebmaster@local>
2019-01-29 11:49:06 +0300

    Меню

--- custom-script.js
+++ custom-script.js
@@ -1,7 +1,17 @@
 jQuery(document).ready(function($)
 {
-    alert('custom-script');
-
     $("body").append("<link id='fromScriptCustom' rel='stylesheet' href='https://vebmaster.github.io/test.css?nocache' type='text/css'>");
     $("ul.navbar-nav").before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
+
+    $('body').append(`
+  <article>
+    <h2>Appended ${++$num}</h2>
+    <p>Using 'single' and "double-quotes" without escaping!</p>
+  </article>
+  
+  <article>
+    <h2>Appended ${++$num}</h2>
+    <p>Using 'single' and "double-quotes" without escaping!</p>
+  </article>
+`);
 });
