70c91acf8eb9714643cc5f7a1922c605ad216a57
vebmaster <vebmaster@local>
2019-01-29 11:04:14 +0300

    Меню

--- custom-script.js
+++ custom-script.js
@@ -1,3 +1,4 @@
 alert('custom-script');
 
+$("body").append("<link id='fromScript' rel='stylesheet' href='https://vebmaster.github.io/test.css?nocache' type='text/css'>");
 $("ul.navbar-nav").before( '<i class="fa fa-bars" aria-hidden="true" class="menu-button" onclick="callback(\'bar\');"></i>' );
