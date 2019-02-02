b96c8ed141868fd88e0d9e990ea29c9feb1f2a15
vebmaster <vebmaster@local>
2019-02-02 16:30:04 +0300

    Карта

--- custom-script.js
+++ custom-script.js
@@ -161,14 +161,14 @@ function addSlider()
 
 function addMap()
 {
-    // $('#userBlockId534004').before(`
-    //     <div id="maps" style="width: 100%; margin: 0px 0px 0px 0px;">
-    //         <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A61e8b4ba3e9937f84472f892980cc30b247d4ae6ebf4fdaaaba2dcd822a8a7f1&amp;width=100%25&amp;height=300&amp;lang=ru_RU&amp;scroll=false"></script>
-    //     </div>
-    // `);
-
     $('#userBlockId534004').before(`
-        <div id="maps"></div>
+        <div id="maps">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A61e8b4ba3e9937f84472f892980cc30b247d4ae6ebf4fdaaaba2dcd822a8a7f1&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=false"></script>
+        </div>
     `);
+
+    // $('#userBlockId534004').before(`
+    //     <div id="maps"></div>
+    // `);
 }
 // addMap()
